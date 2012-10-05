92a49235a994a37bc27406b90edadf5262cc98df
Jean-Tiare LE BIGOT <jtlebigot@socialludia.com>
2012-10-05 16:39:21 -0400

    add pyramid test for delete item

--- tests/functional/pyramid/test_delete_item.py
+++ tests/functional/pyramid/test_delete_item.py
@@ -2,22 +2,40 @@
 
 import unittest, json
 
-TABLE_NAME1 = 'Table-1'
-
+TABLE_NAME = 'Table-HR'
+TABLE_NAME_404 = 'Waldo'
 TABLE_RT = 45
 TABLE_WT = 123
+TABLE_HK_NAME = u'hash_key'
+TABLE_HK_TYPE = u'N'
+TABLE_RK_NAME = u'range_key'
+TABLE_RK_TYPE = u'S'
 
-HASH_KEY = {"AttributeName":"hash_key","AttributeType":"N"}
-RANGE_KEY = {"AttributeName":"range_key","AttributeType":"S"}
+HK_VALUE = u'123'
+RK_VALUE = u'Decode this data if you are a coder'
+
+HK = {TABLE_HK_TYPE: HK_VALUE}
+RK = {TABLE_RK_TYPE: RK_VALUE}
+
+ITEM = {
+    TABLE_HK_NAME: HK,
+    TABLE_RK_NAME: RK,
+    u'relevant_data': {u'B': u'THVkaWEgaXMgdGhlIGJlc3QgY29tcGFueSBldmVyIQ=='},
+}
+
 
 HEADERS = {
-    'x-amz-target': 'DynamoDB_20111205.-----',
+    'x-amz-target': 'DynamoDB_20111205.DeleteItem',
     'content-type': 'application/x-amz-json-1.0',
 }
 
 # Goal here is not to test the full API, this is done by the Boto tests
-class TestTODO(unittest.TestCase):
+class TestDeleteItem(unittest.TestCase):
     def setUp(self):
+        from ddbmock.database.db import DynamoDB
+        from ddbmock.database.table import Table
+        from ddbmock.database.key import PrimaryKey
+
         from ddbmock.database.db import DynamoDB
         from ddbmock import main
         app = main({})
@@ -25,15 +43,30 @@ class TestTODO(unittest.TestCase):
         self.app = TestApp(app)
         DynamoDB().hard_reset()
 
+        hash_key = PrimaryKey(TABLE_HK_NAME, TABLE_HK_TYPE)
+        range_key = PrimaryKey(TABLE_RK_NAME, TABLE_RK_TYPE)
+
+        self.t1 = Table(TABLE_NAME, TABLE_RT, TABLE_WT, hash_key, range_key)
+        DynamoDB().data[TABLE_NAME]  = self.t1
+        self.t1.put(ITEM, {})
+
     def tearDown(self):
         from ddbmock.database.db import DynamoDB
         DynamoDB().hard_reset()
 
-    def test_TODO(self):
+    def test_delete_item_hr(self):
         from ddbmock.database.db import DynamoDB
 
-        request = {}
-        expected = {}
+        request = {
+            "TableName": TABLE_NAME,
+            "Key": {
+                "HashKeyElement":  HK,
+                "RangeKeyElement": RK,
+            },
+        }
+        expected = {
+            u"ConsumedCapacityUnits": 1,
+        }
 
         # Protocol check
         res = self.app.post_json('/', request, HEADERS, status=200)
@@ -41,8 +74,4 @@ class TestTODO(unittest.TestCase):
         self.assertEqual('application/x-amz-json-1.0; charset=UTF-8', res.headers['Content-Type'])
 
         # Live data check
-        data = DynamoDB().data
-        assert TABLE_NAME1 in data
-        table = data[TABLE_NAME1]
-
-        self.assertEqual(TABLE_NAME1, table.name)
+        self.assertEqual({}, self.t1.data[HK_VALUE][RK_VALUE])
