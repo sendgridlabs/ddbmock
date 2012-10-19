a672276e243c07498b827e25b6b06a3a6860242f
Jean-Tiare LE BIGOT <jtlebigot@socialludia.com>
2012-10-19 11:50:15 -0400

    packaging related fixes

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
-
 from setuptools import setup, find_packages
-
-here = os.path.abspath(os.path.dirname(__file__))
 
 install_requires = [
     # d2to1 bootstrap
@@ -13,13 +9,11 @@ install_requires = [
     'pyramid',
     'waitress',
     'voluptuous',
+
+    'setuptools >= 0.6b1',
 ]
 
 tests_requires = [
-    # d2to1 bootstrap
-    'd2to1',
-    'voluptuous',
-    'pyramid',
     'boto',
 
     'nose',
@@ -29,18 +23,17 @@ tests_requires = [
     'webtest',
 ]
 
-setup(name='ddbmock',
-      d2to1=True,
-      keywords='pyramid dynamodb mock',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires,
-      tests_require=tests_requires,
-      test_suite="ddbmock",
-      entry_points = """\
-      [paste.app_factory]
-      main = ddbmock:main
-      """,
+setup(
+    d2to1=True,
+    keywords='pyramid dynamodb mock',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_requires,
+    test_suite="tests",
+    entry_points = """\
+    [paste.app_factory]
+    main = ddbmock:main
+    """,
 )
-
--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,6 @@
 from ddbmock import config
 
 # Much too slow otherwise
-config.DELAY_CREATING = 0.1
-config.DELAY_UPDATING = 0.1
-config.DELAY_DELETING = 0.1
+config.DELAY_CREATING = 0.01
+config.DELAY_UPDATING = 0.01
+config.DELAY_DELETING = 0.01
